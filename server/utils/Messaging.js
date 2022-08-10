6eab481cb0f0415237c25b8b9a83c3e174a7d1ce
Sanskar <64313600+sanskar85@users.noreply.github.com>
2022-08-10 19:29:57 +0530

    Update Messaging.js

--- server/utils/Messaging.js
+++ server/utils/Messaging.js
@@ -3,15 +3,15 @@ const fast2sms = require('fast-two-sms');
 const { ErrorResponse } = require("./error");
 
 exports.SendEmail = async (to, subject, text,attachments) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_SERVICE,
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+ const transporter = nodemailer.createTransport({
+		host: process.env.EMAIL_HOST,
+		port: process.env.EMAIL_PORT,
+		service: process.env.EMAIL_SERVICE,
+		auth: {
+			user: process.env.EMAIL_USERNAME,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	});
 
   const mailOptions = {
     from: process.env.EMAIL_FROM,
